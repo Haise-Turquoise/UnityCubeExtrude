ba2201ed62b2b9b9736fe98c06bd19bb5fb2964f
Haise-Turquoise <y2544liu@uwaterloo.ca>
2022-05-26 11:42:10 -0400

    final version of C++ and dll
    debugged the index issue

--- cube/cube/pch.cpp
+++ cube/cube/pch.cpp
@@ -4,11 +4,10 @@
 
 #define C_API extern "C" __declspec(dllexport)
 // 当使用预编译的头时，需要使用此源文件，编译才能成功。
-C_API float Extrude(float** rawVertices, float* rawNormals, size_t verticesCount)
+C_API void Extrude(float** rawVertices, float* rawNormals, size_t verticesCount)
 {
 	for (size_t i = 0; i < verticesCount * 3; i++)
 	{
-		*(*rawVertices + i) = *(*rawVertices + i) + *rawNormals * 0.01f;
+		*(*rawVertices + i) = *(*rawVertices + i) + *(rawNormals + i) * 0.01f;
 	}
-	return **rawVertices;
 }
